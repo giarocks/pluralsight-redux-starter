9926f7a4c549c7c84f853a19647d2ee90228efb3
gazelle <giarocksfab@gmail.com>
2018-03-02 17:12:53 +0530

    added port reference update
    added port reference update

--- tools/srcServer.js
+++ tools/srcServer.js
@@ -6,7 +6,7 @@ import open from 'open';
 
 /* eslint-disable no-console */
 
-const port = 3003;
+const port =  process.env.PORT || 3003;
 const app = express();
 const compiler = webpack(config);
 
@@ -21,10 +21,13 @@ app.get('*', function(req, res) {
   res.sendFile(path.join( __dirname, '../src/index.html'));
 });
 
+String host = "0.0.0.0";
+
 app.listen(port, function(err) {
   if (err) {
     console.log(err);
   } else {
-    open(`http://localhost:${port}`);
+    console.log(`App running on port ${port}`);
+    open(`http://${host}:${port}`);
   }
 });
